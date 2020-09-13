34658f3c7d0d71764ef71574788ec4219ed3e013
LatHalim <hamitovich86@mail.ru>
2020-09-13 18:23:30 +0300

    18-3

--- ts18/ts18.js
+++ ts18/ts18.js
@@ -1,27 +1,20 @@
 //Первый вариант
 const arr = [10, 12, 15, 21];
 for (let i = 0; i < arr.length; i++) {
-setTimeout(function() {
-console.log('Index: ' + i + ', element: ' + arr[i]);
-}, 3000);
+  setTimeout(function () {
+    console.log('Index: ' + i + ', element: ' + arr[i]);
+  }, 3000);
 }
+
 //Второй вариант
 const arr2 = [10, 12, 15, 21];
-function doSetTimeout(a) {
-    setTimeout(function() { (a); }, 100);
-  }
-for (let a = 0; a < arr2.length; a++) {
-setTimeout(function() {
-console.log('Index: ' + a + ', element: ' + arr2[a]);
-}, 3000);
+for (var i = 0; i < arr2.length; i++) {
+  doScaledTimeout(i);
 }
 
-
-
-  
-  
-
-    
-  
-
-    
+function doScaledTimeout(i) {
+  setTimeout(function () {
+    (i);
+    console.log('Index: ' + i + ', element: ' + arr2[i]);
+  }, 3000);
+}
